e79571ba23e43a77b2f023585da7de8d4fa1caea
Nova <xueshengyuan1118@hotmail.com>
2019-07-16 13:18:13 -0400

    Redesign job model to match json response from github jobs, reset api routes to uitilize new model and create basci functions according to project purposes.

--- models/jobs.js
+++ models/jobs.js
@@ -1,7 +1,20 @@
 module.exports = function (sequelize, DataTypes) {
   var Jobs = sequelize.define("Jobs", {
+    //condition to update or insert 
+    jobId : DataTypes.STRING,
+    //info to render to show exact faved jobs
     title: DataTypes.STRING,
-    location: DataTypes.STRING,
+    type: DataTypes.STRING,
+    city : DataTypes.STRING,
+    state : DataTypes.STRING,
+    url : DataTypes.STRING,
+    created_at : DataTypes.STRING,
+    company : DataTypes.STRING,
+    company_url : DataTypes.STRING,
+    description : DataTypes.TEXT,
+    how_to_apply : DataTypes.STRING,
+    company_logo : DataTypes.STRING,
+    //favedState
     favCount: {
       type: DataTypes.INTEGER,
       defaultValue: 0
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,52 +2,97 @@ var db = require("../models");
 const axios = require("axios");
 
 module.exports = function (app) {
-
+  //main search function  
+  //search api for json response and save all the jobs we got from api to our database
+  //save jobs and update the times these jobs have been searched 
   app.get("/api/jobs/search", function (req, res) {
+    //get api res json and render to client
     let queryUrl = "https://jobs.github.com/positions.json?description=" + req.query.title + "&location=" + req.query.location;
+    axios.get(queryUrl).then(function (response) {
+      res.json(response.data);
 
-    db.Jobs.findOne({
-      where: {
-        title: req.query.title,
-        location: req.query.location
-      }
-    }).then(function (job) {
-      if (job !== null) {
-        db.Jobs.update({
-          searchCount: job.searchCount + 1
-        }, {
-          where: {
-            id: job.id
+
+      
+      //database side
+      //search through database to update or insert 
+      var jobs = response.data;
+      for (let index = 0; index < jobs.length; index++) {
+        db.Jobs.findOne({
+          where : {
+            jobId : jobs[index].id
           }
-        })
-      } else {
-        db.Jobs.create({
-          title: req.query.title,
-          location: req.query.location
+        }).then(function(results){
+          if (results != null) {
+              //this job exists in our database
+              db.Jobs.update({
+                searchCount : searchCount + 1
+              },
+                {
+                where : {
+                  jobId : jobs[index].id
+                }
+              }).then(function(results){
+                if (results.affectedRows == 1) {
+                  console.log("update success");
+                  
+                }
+              });
+          }else{
+            //new job 
+            db.Jobs.create(jobs[index]).then(function(result){
+              if (results.affectedRows ==1 ) {
+                  console.log("insert success");
+                  
+              }
+            });
+          }
         });
       }
     });
 
-    axios.get(queryUrl).then(function (response) {
-      res.json(response.data);
-    });
-
   });
+//get most searched jobs
+ app.get("/api/mostsearched",function(req,res){
+  db.Jobs.findAll({
+    attributes: [[sequelize.fn('COUNT', sequelize.col('searchCount')), 'counts']]//desc or asc
+  }).then(function(results){
+    //res.send(results[0]);
+    // var topFive = [];
+    // for(){
 
-  // Post the relevant job searches from the query to the database
-  app.post("/api/results", function (req, res) {
-    db.jobs.create(req.body).then(function (data) {
-      res.json(data);
-    });
+    // }
+    res.send(topFive);
   });
+ });
+ //get most searched jobs by location or just location
+ app.get("/api/",function(req,res){
 
-  // Get all 
-  app.get("/api/", function (req, res) {
-    db.Example.findAll({}).then(function (data) {
-      res.json(data);
+});
+ //get all faved jobs
+ app.get("/api/favorite",function(req,res){
+    db.Jobs.findAll({
+      where : {
+        favCount : favCount > 0
+      }
+    }).then(function(results){
+      res.send(results);
     });
-  });
-
+});
+app.post("/api/fav",function(req,res){
+    //update fav count here
+    db.Jobs.update({
+      favCount : favCount +1
+    },{
+      where : {
+        id : req.body.id
+      }
+    }).then(function(results){
+        if (results.affectedRows ==1) {
+          console.log("update favscount success");
+          
+        }
+    });
+});
 
 
 };
